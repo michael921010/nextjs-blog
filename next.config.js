21bc856981eca81a7a5cf567725b7f02f87368e2
Michael <s1002573@gmail.com>
2020-11-27 14:57:12 +0800

    include "sass-resources-loader" and "babel-plugin-react-css-modules"

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 const path = require("path");
 const withSass = require("@zeit/next-sass");
+const genericNames = require("generic-names");
+
+const generate = genericNames("[local]__[hash:base64:5]", {
+  context: process.cwd(),
+});
+
+const generateScopedName = (localName, filePath) => {
+  var relativePath = path.relative(process.cwd(), filePath);
+  return generate(localName, relativePath);
+};
 
 module.exports = withSass({
   // 開啟css模組化
@@ -7,10 +17,21 @@ module.exports = withSass({
   cssLoaderOptions: {
     importLoaders: 1,
     // scoped class 格式
-    localIdentName: "[local]__[hash:base64:5]",
+    // localIdentName: "[local]__[hash:base64:5]",
+    getLocalIdent: (context, localIdentName, localName) =>
+      generateScopedName(localName, context.resourcePath),
   },
   webpack: (config) => {
-    config.resolve.modules.push(path.resolve("./"));
+    // 全域性變數和mixin
+    config.module.rules.push({
+      enforce: "pre",
+      test: /.scss$/,
+      loader: "sass-resources-loader",
+      options: {
+        resources: ["./styles/variables.scss"],
+      },
+    });
+
     return config;
   },
 });
